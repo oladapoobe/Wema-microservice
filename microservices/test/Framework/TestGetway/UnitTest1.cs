df4f75c501e18c8654df8e81099958e12d1bc4da
obe oladapo <oladapo_g@outlook.com>
2022-07-12 19:29:42 +0100

    push5678

--- microservices/test/Framework/TestGetway/UnitTest1.cs
+++ microservices/test/Framework/TestGetway/UnitTest1.cs
@@ -1,52 +1,58 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using static TestCustomer.Model;
+using Xunit;
+using static TestGetway.Model;
 
-namespace TestCustomer
+namespace TestGetway
 {
-    [TestClass]
     public class UnitTest1
     {
+        
         static readonly string baseUrl = "http://localhost:5021";
         static HttpClient client = new HttpClient();
 
-        [Test]
+        [Fact]
         public async Task TestMethod1()
         {
             client.BaseAddress = new Uri(baseUrl);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var accessToken = await StateAsync();
-            Assert.IsNotNull(accessToken);
+            var states = await StateAsync();
+            Assert.NotNull(states);
         }
-        [TestMethod]
+    
         static async Task<State> StateAsync()
         {
             var response = await client.GetAsync($"/account/State");
             var Result = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<State>(Result);
         }
-        [TestMethod]
+       
         static async Task<JsonResponseResult> OnboardCustomerAsync(CustomerModel obj)
         {
             var response = await client.PostAsJsonAsync($"/Customer/OnboardCustomer", obj);
             var Result = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<JsonResponseResult>(Result);
-           
+
         }
-        [TestMethod]
+   
         static async Task<LocalGovernment> OnboardCustomerAsync(long stateid)
         {
-            var response = await client.GetAsync($"/account/LocalGovt"+ "/" +stateid);
+            var response = await client.GetAsync($"/account/LocalGovt" + "/" + stateid);
             var Result = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<LocalGovernment>(Result);
 
         }
 
+        static async Task<Customer> GetAllCustomer()
+        {
+            var response = await client.GetAsync($"/account/GetAllCustomer");
+            var Result = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Customer>(Result);
+        }
     }
 }
