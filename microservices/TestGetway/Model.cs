55e701793829aa424b0327b10072a52bacef293e
obe oladapo <oladapo_g@outlook.com>
2022-07-12 11:23:16 +0100

    push4567

---
+++ microservices/TestGetway/Model.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGetway
+{
+    public static class Model
+    {
+        public class CustomerModel
+        {
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public string Pasword { get; set; }
+            public string State { get; set; }
+            public string Lga { get; set; }
+        }
+
+        public class OTPModel
+        {
+            public string PhoneNumber { get; set; }
+        }
+
+        public class SMS
+        {
+            public string PhoneNumber { get; set; }
+            public string Body { get; set; }
+        }
+
+        public class SmsSettings
+        {
+            public string XRapidAPIKey { get; set; }
+            public string XRapidAPIHost { get; set; }
+            public string user { get; set; }
+            public string from { get; set; }
+            public string to { get; set; }
+            public string sms { get; set; }
+            public string method { get; set; }
+            public string classvalue { get; set; }
+            public string password { get; set; }
+            public string RequestUri { get; set; }
+        }
+
+        public class VerifyOTPModel
+        {
+            public string PhoneNumber { get; set; }
+            public string OTP { get; set; }
+        }
+
+        public class Customer
+        {
+            
+            public int Id { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public string Pasword { get; set; }
+            public string State { get; set; }
+            public string Lga { get; set; }
+        }
+
+        public class LocalGovernment
+        {
+           
+            public long Id { get; set; }
+            public long State_id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class OtpLog
+        {
+           
+            public int Id { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Otp { get; set; }
+            public System.DateTime DateCreated { get; set; }
+            public System.DateTime DateExpired { get; set; }
+        }
+
+        public class State
+        {
+          
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class JsonResponseResult
+        {
+            public bool IsSuccessful { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
---
+++ microservices/TestGetway/UnitTest1.cs
@@ -0,0 +1,51 @@
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xunit;
+using static TestGetway.Model;
+
+namespace TestGetway
+{
+    public class UnitTest1
+    {
+        
+        static readonly string baseUrl = "http://localhost:5021";
+        static HttpClient client = new HttpClient();
+
+        [Fact]
+        public async Task TestMethod1()
+        {
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var states = await StateAsync();
+            Assert.NotNull(states);
+        }
+    
+        static async Task<State> StateAsync()
+        {
+            var response = await client.GetAsync($"/account/State");
+            var Result = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<State>(Result);
+        }
+       
+        static async Task<JsonResponseResult> OnboardCustomerAsync(CustomerModel obj)
+        {
+            var response = await client.PostAsJsonAsync($"/Customer/OnboardCustomer", obj);
+            var Result = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<JsonResponseResult>(Result);
+
+        }
+   
+        static async Task<LocalGovernment> OnboardCustomerAsync(long stateid)
+        {
+            var response = await client.GetAsync($"/account/LocalGovt" + "/" + stateid);
+            var Result = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<LocalGovernment>(Result);
+
+        }
+    }
+}
